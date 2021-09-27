8b89ba3d14dbfcd6806cf20ec5d48847f4c5c6f1
Chad Branagan <chad.branagan@gmail.com>
2021-09-26 19:52:10 -0700

    pseudo code and basic question/answers working

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+    //choose the length of the password
+    var passwordLength = window.prompt("How many characters would you like your password to be? Please select between 8-128 characters.")
+
+        console.log(passwordLength)
+    //include lowercase letters?
+    var lowerCase = window.confirm("Would you like your password to include lowercase characters?")
+        
+        console.log(lowerCase)
+    //include uppercase letters?
+    var upperCase = window.confirm("Would you like your password to include Uppercase characters?")
+        
+        console.log(upperCase)
+    
+    //include special characters?
+    var specialCharacter = window.confirm("Would you like your password to include special characters?")
+
+        console.log(specialCharacter)
+    
+    //include numbers?
+    var numbers = window.confirm("Would you like your password to include numbers?")
+
+        console.log(numbers)
+
+    
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +37,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
