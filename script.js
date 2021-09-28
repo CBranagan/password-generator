dbc6630543a65ffb267a330e2dd92955240a0a30
Chad Branagan <chad.branagan@gmail.com>
2021-09-27 17:55:14 -0700

    added arrays for reference to build password.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var lower = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"];
+console.log(lower)
+var upper = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"];
+console.log(upper)
+var symbol = ["!, #, $, %, &, ', (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, ~"];
+console.log(symbol)
+var number = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 0"];
+console.log(number)
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
